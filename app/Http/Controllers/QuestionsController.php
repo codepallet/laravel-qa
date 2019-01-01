6082c0c8fab93f664caa91af2f125e48a712b71e
Kristo Eek <kristo@codepallet.app>
2019-01-01 18:04:47 +0200

    Form validation implemented

--- app/Http/Controllers/QuestionsController.php
+++ app/Http/Controllers/QuestionsController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Question;
 use Illuminate\Http\Request;
+use App\Http\Requests\AskQuestionRequest;
 
 class QuestionsController extends Controller
 {
@@ -29,7 +30,8 @@ class QuestionsController extends Controller
      */
     public function create()
     {
-        //
+        $question = new Question();
+        return view('questions.create')->with('question', $question);
     }
 
     /**
@@ -38,9 +40,11 @@ class QuestionsController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
+    public function store(AskQuestionRequest $request)
     {
-        //
+        // dd('store');
+        $request->user()->questions()->create($request->only('title', 'body'));
+        return redirect()->route('questions.index')->with('success', "Your question has been submitted");
     }
 
     /**
--- resources/views/questions/index.blade.php
+++ resources/views/questions/index.blade.php
@@ -5,9 +5,16 @@
     <div class="row justify-content-center">
         <div class="col-md-12">
             <div class="card">
-                <div class="card-header">All Questions</div>
-
+                <div class="card-header">
+                    <div class="d-flex align-items-center">
+                        <h2>All Questions</h2>
+                        <div class="ml-auto">
+                            <a class ="btn btn-outline-secondary" href="{{ route('questions.create') }}">Ask Question</a>
+                        </div>
+                    </div>
+                </div>
                 <div class="card-body">
+                    @include('layouts._messages')
                     @foreach ($questions as $question)
                         <div class="media">
                             <div class="d-flex flex-column counters">
