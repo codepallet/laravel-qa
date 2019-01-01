7749e88c4ebf4d9cc9afdf0c0fd68cfc958e0050
Kristo Eek <kristo@codepallet.app>
2019-01-01 13:28:36 +0200

    Implemented seeds

--- app/User.php
+++ app/User.php
@@ -28,12 +28,8 @@ class User extends Authenticatable
         'password', 'remember_token',
     ];
 
-    public function question() {
+    public function questions() {
         return $this->hasMany(Question::class);
     }
 
-    public function setTitleAttribute($value) {
-        $this->attributes['title'] = $value;
-        $this->attributes['slug'] = str_slug($value);
-    }
 }
