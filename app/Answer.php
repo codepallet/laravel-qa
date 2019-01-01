9956556fc5da52a803d04cd7083f11656411bf61
Kristo Eek <kristo@codepallet.app>
2019-01-01 23:12:23 +0200

    Implemented answers section to questions

--- app/Answer.php
+++ app/Answer.php
@@ -17,4 +17,17 @@ class Answer extends Model
     public function getBodyHtmlAttribute() {
         return \Parsedown::instance()->text($this->body);
     }
+
+    public function getCreatedDateAttribute() {
+        return $this->created_at->diffForHumans();
+    }
+
+    public static function boot() {
+        parent::boot();
+        static::created(function($answer) {
+            $answer->question->increment('answers_count');
+        });
+    }
+
+
 }
--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,10 +12,18 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call(UsersTableSeeder::class);
-        factory(App\User::class, 3)->create()->each(function($u) {
+        /* ----------------------------------------------
+        *   Create 10 users and for each user it will create
+        *   5 to 30 questions and for each question there will be
+        *   3 to 15 answers
+        ------------------------------------------------*/
+        factory(App\User::class, 10)->create()->each(function($u) {
             $u->questions()->saveMany(
-                factory(App\Question::class, rand(1, 5))->make()
-            );
+                factory(App\Question::class, rand(5, 30))->make()
+            )
+            ->each(function ($q) {
+                $q->answers()->saveMany(factory(App\Answer::class, rand(3, 15))->make());
+            });
         });
 
     }
